6f5d9d958b3612d4eae23bc45c360e29e1860558
lin <lchaoqun@sina.cn>
2017-09-17 11:09:20 +0800

    auto open when start

--- webpack.watch.config.js
+++ webpack.watch.config.js
@@ -9,39 +9,49 @@ var postcssConfig = require('./postcss.config');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractLESS = new ExtractTextPlugin('stylesheets/[name].less.css');
 const extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
-module.exports =a =  webpackMerge(commonConfig, {
-  // entry: {
-  //   vendor:['vue/dist/vue.common.js','vue-resource','vue-router',
-  //     'moment','element-ui',path.resolve(ROOT_PATH, 'ueditor','ueditor.all.js','lodash')]
-  // },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: extractCSS.extract([ 'css-loader', 'postcss-loader' ])
-      },
-      {
-        test: /\.less$/,
-        use: extractLESS.extract([ 'css-loader', {
-          loader: 'postcss-loader',
-          options: {
-            plugins: postcssConfig.plugins
-          }
-        }, 'less-loader' ]),
-        include: CSS_PATH
-      }
+module.exports = a = webpackMerge(commonConfig, {
+    // entry: {
+    //   vendor:['vue/dist/vue.common.js','vue-resource','vue-router',
+    //     'moment','element-ui',path.resolve(ROOT_PATH, 'ueditor','ueditor.all.js','lodash')]
+    // },
+    devtool: 'source-map',
+    module: {
+        rules: [
+
+            {
+                test: /\.(js|vue)$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [ROOT_PATH],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract(['css-loader', 'postcss-loader'])
+            },
+            {
+                test: /\.less$/,
+                use: extractLESS.extract(['css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: postcssConfig.plugins
+                    }
+                }, 'less-loader']),
+                include: CSS_PATH
+            }
+        ]
+    },
+    //输出的文件名 合并以后的js会命名为bundle.js
+    output: {
+        path: BUILD_PATH,
+        publicPath: "/",
+        filename: "[name].bundle.js"
+    },
+    plugins: [
+        extractCSS,
+        extractLESS
+        // new webpack.NoErrorsPlugin()
     ]
-  },
-  //输出的文件名 合并以后的js会命名为bundle.js
-  output: {
-    path: BUILD_PATH,
-    publicPath:"/",
-    filename: "[name].bundle.js"
-  },
-  plugins: [
-    extractCSS,
-    extractLESS
-    // new webpack.NoErrorsPlugin()
-  ]
 })
