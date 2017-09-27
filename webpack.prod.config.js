2d5686d31eb8993a81791dfffa082281441a6d3e
lin <lchaoqun@sina.cn>
2017-09-27 14:55:48 +0800

    code style

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,15 +1,15 @@
-var path = require('path');
-var webpack = require('webpack');
-var WebpackMd5Hash = require('webpack-md5-hash');
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.config.js');
-var BUILD_PATH = path.resolve(__dirname, 'dist');
-var ROOT_PATH = path.resolve(__dirname, 'public');
-var CSS_PATH = path.resolve(ROOT_PATH, 'css');
-var postcssConfig = require('./postcss.config');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractLESS = new ExtractTextPlugin('stylesheets/[contenthash].[name].less.css');
-const extractCSS = new ExtractTextPlugin('stylesheets/[contenthash].[name].css');
+const path = require("path");
+const webpack = require("webpack");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const webpackMerge = require("webpack-merge");
+const commonConfig = require("./webpack.config.js");
+const BUILD_PATH = path.resolve(__dirname, "dist");
+const ROOT_PATH = path.resolve(__dirname, "public");
+const CSS_PATH = path.resolve(ROOT_PATH, "css");
+const postcssConfig = require("./postcss.config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLESS = new ExtractTextPlugin("stylesheets/[contenthash].[name].less.css");
+const extractCSS = new ExtractTextPlugin("stylesheets/[contenthash].[name].css");
 module.exports = webpackMerge(commonConfig, {
     // entry: {
     //   vendor:['vue/dist/vue.common.js','vue-resource','vue-router']
@@ -19,16 +19,16 @@ module.exports = webpackMerge(commonConfig, {
         rules: [
             {
                 test: /\.css$/,
-                use: extractCSS.extract(['css-loader', 'postcss-loader'])
+                use: extractCSS.extract(["css-loader", "postcss-loader"])
             },
             {
                 test: /\.less$/,
-                use: extractLESS.extract(['css-loader', {
-                    loader: 'postcss-loader',
+                use: extractLESS.extract(["css-loader", {
+                    loader: "postcss-loader",
                     options: {
                         plugins: postcssConfig.plugins
                     }
-                }, 'less-loader']),
+                }, "less-loader"]),
                 include: CSS_PATH
             }
         ]
@@ -43,9 +43,9 @@ module.exports = webpackMerge(commonConfig, {
     plugins: [
         new WebpackMd5Hash(),
         new webpack.DefinePlugin({
-            'process.env': {
+            "process.env": {
                 //注意一个单引号一个双引号…… 这里是要将 "production" 替换到文件里面
-                NODE_ENV: '"production"'
+                NODE_ENV: "\"production\""
             }
         }),
         new webpack.LoaderOptionsPlugin({
@@ -66,4 +66,4 @@ module.exports = webpackMerge(commonConfig, {
         extractCSS,
         extractLESS
     ]
-})
+});
--- webpack.watch.config.js
+++ webpack.watch.config.js
@@ -1,44 +1,44 @@
-var path = require('path');
-var webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
-const commonConfig = require('./webpack.config.js');
-var BUILD_PATH = path.resolve(__dirname, 'dist');
-var ROOT_PATH = path.resolve(__dirname, 'public');
-var CSS_PATH = path.resolve(ROOT_PATH, 'css');
-var postcssConfig = require('./postcss.config');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractLESS = new ExtractTextPlugin('stylesheets/[name].less.css');
-const extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
+const path = require("path");
+const webpack = require("webpack");
+const webpackMerge = require("webpack-merge");
+const commonConfig = require("./webpack.config.js");
+const BUILD_PATH = path.resolve(__dirname, "dist");
+const ROOT_PATH = path.resolve(__dirname, "public");
+const CSS_PATH = path.resolve(ROOT_PATH, "css");
+const postcssConfig = require("./postcss.config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractLESS = new ExtractTextPlugin("stylesheets/[name].less.css");
+const extractCSS = new ExtractTextPlugin("stylesheets/[name].css");
 module.exports = a = webpackMerge(commonConfig, {
     // entry: {
     //   vendor:['vue/dist/vue.common.js','vue-resource','vue-router',
     //     'moment','element-ui',path.resolve(ROOT_PATH, 'ueditor','ueditor.all.js','lodash')]
     // },
-    devtool: 'source-map',
+    devtool: "source-map",
     module: {
         rules: [
 
             {
                 test: /\.(js|vue)$/,
-                loader: 'eslint-loader',
-                enforce: 'pre',
+                loader: "eslint-loader",
+                enforce: "pre",
                 include: [ROOT_PATH],
                 options: {
-                    formatter: require('eslint-friendly-formatter')
+                    formatter: require("eslint-friendly-formatter")
                 }
             },
             {
                 test: /\.css$/,
-                use: extractCSS.extract(['css-loader', 'postcss-loader'])
+                use: extractCSS.extract(["css-loader", "postcss-loader"])
             },
             {
                 test: /\.less$/,
-                use: extractLESS.extract(['css-loader', {
-                    loader: 'postcss-loader',
+                use: extractLESS.extract(["css-loader", {
+                    loader: "postcss-loader",
                     options: {
                         plugins: postcssConfig.plugins
                     }
-                }, 'less-loader']),
+                }, "less-loader"]),
                 include: CSS_PATH
             }
         ]
@@ -54,4 +54,4 @@ module.exports = a = webpackMerge(commonConfig, {
         extractLESS
         // new webpack.NoErrorsPlugin()
     ]
-})
+});
